bcbb4d89dc0a724cacadc7582eaeb2cef08dafca
Sampadak-04 <kothari.sampada33@gmail.com>
2021-09-02 19:14:18 +0530

    image inserted

--- src/main/java/com/exam/DAO/DBconnection.java
+++ src/main/java/com/exam/DAO/DBconnection.java
@@ -13,7 +13,7 @@ public class DBconnection {
 			//connection to data Base
 
 
-			con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","12345678");
+			con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","sampada","Sampada04");
 
 
 		} catch (ClassNotFoundException e) {
